84929e2e95d72d117844af1f22dd13015fb1fed9
sahilgyanchandani <gyanchandanisahil8@gmail.com>
2023-09-04 18:28:56 +0530

    Google Changes and minor changes

---
+++ src/Acme.ChatApp.Application.Contracts/GoogleLogin/ExternalAuthDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Acme.ChatApp.GoogleLogin
+{
+    public class ExternalAuthDto
+    {
+        public const string PROVIDER = "google";
+
+        [Required]
+        public string IdToken { get; set; }
+
+    }
+}
---
+++ src/Acme.ChatApp.Application/GoogleLogin/GoogleAuthenticationAppService.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.ModelBinding;
+using Volo.Abp;
+using Volo.Abp.Application.Services;
+using Volo.Abp.DependencyInjection;
+using static Google.Apis.Auth.GoogleJsonWebSignature;
+
+namespace Acme.ChatApp.GoogleLogin
+{
+    public class GoogleAuthenticationAppService:ApplicationService,ITransientDependency
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public GoogleAuthenticationAppService(UserManager<IdentityUser> userManager,IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public async Task<IdentityUser> AuthenticateGoogleUserAsync(ExternalAuthDto request)
+        {
+            if (!IsValid(request))
+            {
+                throw new UserFriendlyException("Invalid input data.");
+            }
+
+            var user = await AuthenticateAndCreateUserAsync(request);
+            return user;
+        }
+
+        private async Task<IdentityUser> AuthenticateAndCreateUserAsync(ExternalAuthDto request)
+        {
+            var payload = await ValidateAsync(request.IdToken, new ValidationSettings
+            {
+                Audience = new[] { _configuration["Authentication:Google:ClientId"] }
+            });
+
+            var user = await _userManager.FindByLoginAsync(ExternalAuthDto.PROVIDER, payload.Subject);
+
+            if (user == null)
+            {
+                user = await _userManager.FindByEmailAsync(payload.Email);
+
+                if (user == null)
+                {
+                    user = new IdentityUser
+                    {
+                        Email = payload.Email,
+                        UserName = payload.GivenName,
+                        Id = payload.Subject,
+                    };
+                }
+
+                var userName = await _userManager.FindByNameAsync(payload.GivenName);
+                if (userName != null)
+                {
+                    string newUserName = payload.GivenName;
+                    int count = 1;
+                    while (userName != null)
+                    {
+                        newUserName = $"{payload.GivenName}{count:D2}";
+                        userName = await _userManager.FindByNameAsync(newUserName);
+                        count++;
+                    }
+                    user.UserName = newUserName;
+                    await _userManager.UpdateAsync(user);
+                }
+                await _userManager.CreateAsync(user);
+                var info = new UserLoginInfo(ExternalAuthDto.PROVIDER, payload.Subject, ExternalAuthDto.PROVIDER.ToUpperInvariant());
+                await _userManager.AddLoginAsync(user, info);
+            }
+
+            return user;
+        }
+
+        private string CreateToken(IdentityUser user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                 //new Claim("UserId", user.UserID.ToString())
+                //new Claim(ClaimTypes.Role,"User")
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+            var token = new JwtSecurityToken
+                (
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+
+        }
+
+        private bool IsValid(ExternalAuthDto request)
+        {
+            return !string.IsNullOrWhiteSpace(request.IdToken);
+        }
+    }
+}
