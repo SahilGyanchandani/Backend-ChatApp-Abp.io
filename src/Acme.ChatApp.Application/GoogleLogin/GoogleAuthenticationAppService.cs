a0931e119f2d543e0e379378cdaab8e665f8dcf9
sahilgyanchandani <gyanchandanisahil8@gmail.com>
2023-09-12 11:34:09 +0530

    Relationship  and Minor Changes

--- src/Acme.ChatApp.Application/GoogleLogin/GoogleAuthenticationAppService.cs
+++ src/Acme.ChatApp.Application/GoogleLogin/GoogleAuthenticationAppService.cs
@@ -14,107 +14,135 @@ using System.Web.Http.ModelBinding;
 using Volo.Abp;
 using Volo.Abp.Application.Services;
 using Volo.Abp.DependencyInjection;
+using Volo.Abp.Identity;
 using static Google.Apis.Auth.GoogleJsonWebSignature;
 
 namespace Acme.ChatApp.GoogleLogin
 {
     //[Authorize]
-    public class GoogleAuthenticationAppService:ApplicationService,ITransientDependency
-    {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly IConfiguration _configuration;
+    //public class GoogleAuthenticationAppService:ApplicationService,ITransientDependency
+    //{
+    //    private readonly UserManager<IdentityUser> _userManager;
+    //    private readonly IConfiguration _configuration;
 
-        public GoogleAuthenticationAppService(UserManager<IdentityUser> userManager,IConfiguration configuration)
-        {
-            _userManager = userManager;
-            _configuration = configuration;
-        }
+    //    public GoogleAuthenticationAppService(UserManager<IdentityUser> userManager,IConfiguration configuration)
+    //    {
+    //        _userManager = userManager;
+    //        _configuration = configuration;
+    //    }
 
-        public async Task<IdentityUser> AuthenticateGoogleUserAsync(ExternalAuthDto request)
-        {
-            if (!IsValid(request))
-            {
-                throw new UserFriendlyException("Invalid input data.");
-            }
+        //    public async Task<IdentityUser> AuthenticateGoogleUserAsync(ExternalAuthDto request)
+        //    {
+        //        if (!IsValid(request))
+        //        {
+        //            throw new UserFriendlyException("Invalid input data.");
+        //        }
 
-            var user = (await AuthenticateAndCreateUserAsync(request));
-            //var accessToken = CreateAccessToken(CreateJwtClaims(loginResult.Identity));
-            return user;
-        }
+        //        var user = (await AuthenticateAndCreateUserAsync(request));
+        //        //var accessToken = CreateAccessToken(CreateJwtClaims(loginResult.Identity));
+        //        return user;
+        //    }
 
-        private async Task<IdentityUser> AuthenticateAndCreateUserAsync(ExternalAuthDto request)
-        {
-            var payload = await ValidateAsync(request.IdToken, new ValidationSettings
-            {
-                Audience = new[] { _configuration["Authentication:Google:ClientId"] }
-            });
+        //    private async Task<IdentityUser> AuthenticateAndCreateUserAsync(ExternalAuthDto request)
+        //    {
+        //        var payload = await ValidateAsync(request.IdToken, new ValidationSettings
+        //        {
+        //            Audience = new[] { _configuration["Authentication:Google:ClientId"] }
+        //        });
 
-            var user = await _userManager.FindByLoginAsync(ExternalAuthDto.PROVIDER, payload.Subject);
+        //        var user = await _userManager.FindByLoginAsync(ExternalAuthDto.PROVIDER, payload.Subject);
 
-            if (user == null)
-            {
-                user = await _userManager.FindByEmailAsync(payload.Email);
+        //        if (user == null)
+        //        {
+        //            user = await _userManager.FindByEmailAsync(payload.Email);
 
-                if (user == null)
-                {
-                    user = new IdentityUser
-                    {
-                        Email = payload.Email,
-                        UserName = payload.GivenName,
-                        Id = payload.Subject,
-                    };
-                }
+        //            if (user == null)
+        //            {
+        //                user = new IdentityUser
+        //                {
+        //                    Email = payload.Email,
+        //                    UserName = payload.GivenName,
+        //                    Id = payload.Subject,
+        //                };
+        //            }
 
-                var userName = await _userManager.FindByNameAsync(payload.GivenName);
-                if (userName != null)
-                {
-                    string newUserName = payload.GivenName;
-                    int count = 1;
-                    while (userName != null)
-                    {
-                        newUserName = $"{payload.GivenName}{count:D2}";
-                        userName = await _userManager.FindByNameAsync(newUserName);
-                        count++;
-                    }
-                    user.UserName = newUserName;
-                    await _userManager.UpdateAsync(user);
-                }
-                await _userManager.CreateAsync(user);
-                var info = new UserLoginInfo(ExternalAuthDto.PROVIDER, payload.Subject, ExternalAuthDto.PROVIDER.ToUpperInvariant());
-                await _userManager.AddLoginAsync(user, info);
-            }
+        //            var userName = await _userManager.FindByNameAsync(payload.GivenName);
+        //            if (userName != null)
+        //            {
+        //                string newUserName = payload.GivenName;
+        //                int count = 1;
+        //                while (userName != null)
+        //                {
+        //                    newUserName = $"{payload.GivenName}{count:D2}";
+        //                    userName = await _userManager.FindByNameAsync(newUserName);
+        //                    count++;
+        //                }
+        //                user.UserName = newUserName;
+        //                await _userManager.UpdateAsync(user);
+        //            }
+        //            await _userManager.CreateAsync(user);
+        //            var info = new UserLoginInfo(ExternalAuthDto.PROVIDER, payload.Subject, ExternalAuthDto.PROVIDER.ToUpperInvariant());
+        //            await _userManager.AddLoginAsync(user, info);
+        //        }
 
-            return user;
-        }
+        //        return user;
+        //    }
 
-        private string CreateToken(IdentityUser user)
-        {
-            List<Claim> claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Email, user.Email),
-                new Claim(ClaimTypes.NameIdentifier, user.Id),
-                 //new Claim("UserId", user.UserID.ToString())
-                //new Claim(ClaimTypes.Role,"User")
-            };
+        //    private string CreateToken(IdentityUser user)
+        //    {
+        //        List<Claim> claims = new List<Claim>
+        //        {
+        //            new Claim(ClaimTypes.Email, user.Email),
+        //            new Claim(ClaimTypes.NameIdentifier, user.Id),
+        //             //new Claim("UserId", user.UserID.ToString())
+        //            //new Claim(ClaimTypes.Role,"User")
+        //        };
 
-            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+        //        var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
 
-            var token = new JwtSecurityToken
-                (
-                    claims: claims,
-                    expires: DateTime.Now.AddDays(1),
-                    signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
-                );
+        //        var token = new JwtSecurityToken
+        //            (
+        //                claims: claims,
+        //                expires: DateTime.Now.AddDays(1),
+        //                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+        //            );
 
-            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+        //        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
 
-            return jwt;
+        //        return jwt;
 
-        }
+        //    }
 
-        private bool IsValid(ExternalAuthDto request)
-        {
-            return !string.IsNullOrWhiteSpace(request.IdToken);
-        }
-    }
+        //    private bool IsValid(ExternalAuthDto request)
+        //    {
+        //        return !string.IsNullOrWhiteSpace(request.IdToken);
+        //    }
+
+
+        //public class GoogleAuthenticationProvider : ExternalLoginInfo
+        //{
+        //    public override string Name => "Google";
+
+        //    public GoogleAuthenticationProvider(IServiceProvider serviceProvider)
+        //        : base(serviceProvider)
+        //    {
+        //    }
+
+        //    public override async Task<ExternalLoginUserInfo> GetUserInfoAsync(string idToken)
+        //    {
+        //        // Use Google's API to validate the idToken and fetch user information
+        //        // You may need to use a library like Google.Apis.Auth
+        //        // Extract user information (e.g., email, name) from the response
+
+        //        // Create an ExternalLoginUserInfo object with user information
+        //        var userInfo = new ExternalLoginUserInfo
+        //        {
+        //            Name = "John Doe",
+        //            Email = "john.doe@example.com"
+        //        };
+
+        //        return userInfo;
+        //    }
+        //}
+    //}
 }
--- src/Acme.ChatApp.Domain/Messages/Message.cs
+++ src/Acme.ChatApp.Domain/Messages/Message.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +14,14 @@ namespace Acme.ChatApp.Messages
     {
         [Key]
         public int MessageId { get; set; }
+        [ForeignKey(nameof(SenderId))]
         public Guid SenderId { get; set; } //SenderID Of User
+        public IdentityUser? Sender { get; set; }
+        [ForeignKey(nameof(SenderId))]
         public Guid ReceiverId { get; set; } //ReceiverID of User
+        public IdentityUser? Receiver { get; set; }
         public string Content { get; set; }
-        public DateTime Timestamp { get; set; }
+        public DateTime Timestamp { get; set; }   
 
     }
 }
